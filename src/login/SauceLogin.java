5ba9d8a748b7157f3ca4f60144d3d2d6336c3a2b
dzcorovic <dzenanacorovic@yahoo.com>
2020-06-10 20:34:41 +0200

    SauceDemo

---
+++ src/login/SauceLogin.java
@@ -0,0 +1,49 @@
+package login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SauceLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\Nahla_automation\\vjezba\\SauceDemo\\executable\\chromedriver.exe");
+	
+	    WebDriver driver = new ChromeDriver(); 
+	    
+	    // step 1 open sauce page
+	    
+	    driver.get("https://www.saucedemo.com/");
+	    
+	    // step 2 open
+	    
+	   // driver.navigate().to("https://www.saucedemo.com/");
+	    
+	   // find user-name element 
+	    
+	   WebElement username= driver.findElement(By.id("user-name"));
+	   
+	   // find element password
+	   
+	   WebElement password= driver.findElement(By.id("password"));
+	   
+	   // find Login button
+	   
+	   WebElement logButton = driver.findElement(By.className("btn_action"));
+	   
+	   // sent username and password
+	   username.sendKeys("standard_user");
+	   password.sendKeys("secret_sauce");
+	   
+	   
+	   // click on login button
+	   
+	   logButton.click();
+	   
+	   
+	}
+
+}
